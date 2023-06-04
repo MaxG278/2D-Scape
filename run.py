d68ca7f90d157067ddf536a9b7e9e63a2ee5b5e8
Max Götte <maxgotte@Maxs-Mac-mini.fritz.box>
2023-06-04 17:33:51 +0200

    added game class

---
+++ run.py
@@ -0,0 +1,8 @@
+from src.game import Game
+
+def main():
+    print("Game started")
+    game = Game()
+
+if __name__ == "__main__":
+    main()
---
+++ src/game.py
@@ -0,0 +1,22 @@
+"""
+eine klasse mit dem namen game, die soll eine funktion run eine funktion start und exit
+ haben
+die sollten erst einmal leer sein. Die soll eine variable running haben, 
+und diese sollen in der run.py initialisiert werden.
+"""
+
+
+class Game:
+    running = False
+    
+    
+    def start(self):
+        pass
+    
+
+    def run(self):
+        pass
+
+    def exit(self):
+        pass
+
